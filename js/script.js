0f1942bca571fbf2b38cd809d6dc9f99f31528ee
Kekray <kekray777@gmail.com>
2022-03-19 17:01:07 +0300

    Regular expressions

--- js/script.js
+++ js/script.js
@@ -338,12 +338,10 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
-            // на данный момент у нас примитивный способ формата строки в число
-            // в будущем будут использоваться регулярные выражения
+        if (offset == +width.replace(/\D/g, '') * (slides.length - 1)){
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += +width.replace(/\D/g, '');
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -361,9 +359,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0){
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = +width.replace(/\D/g, '') * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= +width.replace(/\D/g, '');
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -385,7 +383,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = offset = +width.replace(/\D/g, '') * (slideTo - 1);
     
             slidesField.style.transform = `translateX(-${offset}px)`;
     
